9431ac5c0f0550e8337743628251eb9a3d4286b2
jih213 <58736269+jih213@users.noreply.github.com>
2020-02-05 22:36:36 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -288,10 +288,10 @@ namespace mbitbot {
     //% blockId=Get_ThingSpeak_field1 block="Get field1"
     //% weight=10
     export function Get_field1(): number {
-        //TS_txt = serial.readUntil("{")
-        //EsResponse = serial.readBuffer(512)
-        //Esnum = EsResponse.getNumber(NumberFormat.Int8LE, 10)
-        let Esnum = 69
+        Ts_txt=serial.readUntil(":")
+        EsResponse = serial.readBuffer(20)
+        let Esnum = EsResponse.getNumber(NumberFormat.Int8LE, 10)
+        //let Esnum = 69
         //TS_txt = EsResponse
         return Esnum
     }
